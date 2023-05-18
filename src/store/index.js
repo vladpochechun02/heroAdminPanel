39854c975a4fef627a5ecd7f1bcd37e5bc675363
Vladyslav <kpigoodvlad@gmail.com>
2023-05-18 17:28:34 +0300

    Разбил папку redusers на 2 файла, heroes и filters и оптимизировал код

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,7 @@
-// import { createStore } from 'redux';
-// import reducer from '../reducers';
+import { createStore, combineReducers } from 'redux';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
 
-// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-// export default store;
-
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from '../reducers';
-
-const store = configureStore({
-  reducer,
-  devTools: process.env.NODE_ENV !== 'production', // Включение Redux DevTools только в режиме разработки
-});
+const store = createStore(combineReducers({heroes, filters}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
